77868b483a6634c0e66db839d7acc81feee98ad3
Addisonbean <addisonbean@gmail.com>
2014-03-22 20:20:32 -0500

    tweaked css

--- results.php
+++ results.php
@@ -15,9 +15,11 @@ $unused_files = $_POST['unused_files'];
 $unused_files_count = explode(",", $unused_files);
 if ($round >= 5) {
 	$action = "done";
+	$btn_text = "Finish";
 } else {
 	$round += 1;
 	$action = "play";
+	$btn_text = "Next Round";
 }
 
 include 'php/layout.php';
@@ -27,15 +29,16 @@ include 'php/layout.php';
 		<div id="perror">
 			<h2>Your percent error:</h2>
 			<p><?= $perror ?>%</p>
+			<form action="<?= $action ?>.php" method="post">
+				<input type="hidden" name="perror_list" value="<?= $perror_list ?>">
+				<input type="hidden" name="unused_files" value="<?= $unused_files ?>">
+				<input type="hidden" name="used_file" value="<?= $file_name ?>">
+				<input type="hidden" name="round" value="<?= $round ?>">
+				<button class="btn" type="submit"><?= $btn_text ?></button>
+			</form>
 		</div>
 
-		<form action="<?= $action ?>.php" method="post">
-			<input type="hidden" name="perror_list" value="<?= $perror_list ?>">
-			<input type="hidden" name="unused_files" value="<?= $unused_files ?>">
-			<input type="hidden" name="used_file" value="<?= $file_name ?>">
-			<input type="hidden" name="round" value="<?= $round ?>">
-			<button class="btn" type="submit">Next Round</button>
-		</form>
+		
 	</div>
 </body>
 </html>
